1ef190b233d5cef065be689c35c1b6e050cbb784
Main <celeste.ornato1@gmail.com>
2024-05-02 00:14:48 +0200

    Cleaned up config

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,8 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  
+  # Set refresh rate to 60Hz at bootup to counter a manufacturing defect on my screen
   boot.kernelParams = [
     "drm.edid_firmware=eDP-1:edid/1920x1080.bin"
   ];
@@ -23,7 +25,6 @@
   networking.hostName = "nixos"; # Define your hostname.
 
   # nvidia
-  services.xserver.videoDrivers = ["nvidia"];
   hardware.opengl = {
     enable = true;
     driSupport = true;
@@ -85,34 +86,56 @@
     LANG = "en_GB.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.displayManager.startx.enable = true;
-  # Enable the XFCE Desktop Environment.
-  # services.xserver.displayManager.lightdm.enable = true;
-  # services.xserver.desktopManager.xfce.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "fr";
-    xkbVariant = "";
-  };
-
   # Configure console keymap
   console.keyMap = "fr";
 
-  # Enable CUPS to print documents.
-  services.avahi = {
-      enable = true;
-      nssmdns = true;
-      openFirewall = true;
+  services = {
+      # Cups does not seem to work but you never know
+      printing = {
+        enable = true;
+        allowFrom = [ "all" ];
+        browsing = true;
+        defaultShared = true;
+        openFirewall = true;
+        drivers = [pkgs.hplipWithPlugin];
+      };
+      avahi = {
+        enable = true;
+        nssmdns = true;
+        openFirewall = true;
+        publish = {
+          enable = true;
+          userServices = true;
+        };
+      };
+
+      # Bluetooth
+      blueman.enable = true;
+      
+      # Sound
+      pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+      };
+
+      # Touchpad
+      xserver.libinput.enable = true;
+
+      xserver = {
+        videoDrivers = ["nvidia"];
+        enable = true;
+        displayManager.startx.enable = true;
+        layout = "fr";
+        xkbVariant = "";
+      };
+
+      openssh.enable = true;
   };
-  services.printing.enable = true;
-  services.printing.drivers = [pkgs.hplipWithPlugin];
   
   # Bluetooth
   hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
   hardware.bluetooth.powerOnBoot = true;
 
 
@@ -120,21 +143,6 @@
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.main = {
@@ -231,8 +239,6 @@
 
   # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
